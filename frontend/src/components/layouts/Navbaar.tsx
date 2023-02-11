d858eebaf438374bf4fa1d0b42e5ae5cb499d50e
niteshKrr <nkumar35101@gmail.com>
2023-02-11 12:44:28 +0530

    tailwind and nextUI setup

---
+++ frontend/src/components/layouts/Navbaar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+
+const Navbaar = () => {
+  return (
+      <Navbar>
+        <Navbar.Brand>
+          <Text b color="inherit" hideIn="xs">
+            ACME
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content hideIn="xs">
+          <Navbar.Link href="#">Features</Navbar.Link>
+          <Navbar.Link isActive href="#">
+            Customers
+          </Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Company</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="#">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat as={Link} href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+  );
+};
+
+export default Navbaar;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navbaar from "@/components/layouts/Navbaar";
+import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Navbaar/>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
